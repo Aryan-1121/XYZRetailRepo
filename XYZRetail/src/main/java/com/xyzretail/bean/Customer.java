ca2875e8360de835350301525b33da65ca52b91c
Aryan <aryanmaurya879@gmail.com>
2022-07-26 14:36:17 +0530

    sql file added

---
+++ XYZRetail/src/main/java/com/xyzretail/bean/Customer.java
@@ -0,0 +1,17 @@
+package com.xyzretail.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Data
+public class Customer {
+	
+	private String userName;
+	private String userPassord;
+	
+}

5bdfc923c5cb53dca3b479f63c0ac155e68b3342
Rishabh Dev <rishabhdev444@gmail.com>
2022-07-29 13:05:33 +0530

    CutomerPresentation added and implemented
    Updates presentation

--- XYZRetail/src/main/java/com/xyzretail/presentation/CustomerPresentationImpl.java
+++ XYZRetail/src/main/java/com/xyzretail/presentation/CustomerPresentationImpl.java
@@ -6,31 +6,28 @@ import java.util.Scanner;
 import com.xyzretail.bean.Customer;
 import com.xyzretail.bean.ItemDetails;
 import com.xyzretail.persistence.CustomerDao;
-
 import com.xyzretail.persistence.CustomerDaoImpl;
 import com.xyzretail.service.ItemsService;
 import com.xyzretail.service.ItemsServiceImpl;
 
-public class ItemsPresentationImpl implements ItemsPresentation{
-	
+public class CustomerPresentationImpl implements CustomerPresentation {
+
 	private ItemsService itemsService=new ItemsServiceImpl();
 	private CustomerDao CustomerDao= new CustomerDaoImpl();
 	
 	@Override
-	public void showMenu() {
+	public void showMenuCustomer() {
 		System.out.println("=============================");
 		System.out.println("1. Enter your details for Registration ");
 		System.out.println("2. Enter you details for Login");
-//		System.out.println("3. Show All Items");
-		System.out.println("3. Do you wants to shop?");
-		System.out.println("4. Generate Bill");
-		System.out.println("5. Exit");
+		System.out.println("3. Exit");
 		System.out.println("================================");
 		
 	}
-	
+
 	@Override
-	public void performMenu(int choice) {
+	public void performMenuCustomer(int choice) {
+		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
 		try {
 		switch(choice) {
@@ -84,6 +81,7 @@ public class ItemsPresentationImpl implements ItemsPresentation{
 				List<ItemDetails> items=itemsService.getAllItems();
 				System.out.println("Available items:");
 				System.out.println("Item Name  \t Cost of each Unit \t Avaliable Quantity");
+				
 				for(ItemDetails item:items) {
 					System.out.println(item.getItemName()+"\t"+item.getItemPrice()+"\t"+item.getAvailableQuantity());
 				}
@@ -93,48 +91,8 @@ public class ItemsPresentationImpl implements ItemsPresentation{
 				System.out.println("Please Login Again To See All Items !!!");
 			}
 			break;
-			
-//		case 3: 
-//			List<Item> items=itemsService.getAllItems();
-//			System.out.println("Available items:");
-//			System.out.println("Item Name  \t Cost of each Unit \t Avaliable Quantity");
-//			for(Item item:items) {
-//				System.out.println(item.getItemName()+"\t"+item.getItemPrice()+"\t"+item.getItemLeft());
-//			}
-//			break;	
 	
 		case 3:
-//			System.out.println("Do you wants to shop : If YES enter 1, If NO enter 0");
-			int ch=1;
-			while(ch!=0) {
-				
-				System.out.println("Enter Item Id you wants to buy: ");
-				String id=sc.next();
-				System.out.println("Enter the quantity you wants to buy : ");
-				int requiredQuantity=sc.nextInt();
-				
-				ItemDetails item=itemsService.searchItemsById(id,requiredQuantity);
-				if(item!=null) {
-					System.out.println("Item added to cart is successfully : "+item+" : "+requiredQuantity);
-				}
-				System.out.println("Do you wants to shop : If YES enter 1, If NO enter 0");
-				ch=sc.nextInt();
-			}
-		
-			break;
-		case 4: 
-			System.out.println("Your Total Bill Amount is : ");
-			double itemsBill=itemsService.generateBill();
-			if(itemsBill!=0) {
-				System.out.println(itemsBill);
-			}
-			else
-				System.out.println("");
-			break;
-		
-		
-	
-		case 5:
 			System.out.println("\n*************** Thanks for using our Shopping Basket Application!! ************");
 			System.exit(0);
 			
@@ -149,5 +107,8 @@ public class ItemsPresentationImpl implements ItemsPresentation{
 		catch(Exception exception) {
 			System.out.println("");
 		}
-}
+		
+		
+	}
+
 }

5cbc8668d9548b6beffacdba153353d32a2fa06a
Nagalakshmi S <nagal@LAPTOP-NEFDGL5>
2022-07-29 13:06:26 +0530

    added transaction dao

---
+++ XYZRetail/src/main/java/com/xyzretail/persistence/TransactionDao.java
@@ -0,0 +1,10 @@
+package com.xyzretail.persistence;
+
+import com.xyzretail.bean.Customer;
+import com.xyzretail.bean.Transaction;
+
+public interface TransactionDao {
+
+		Transaction getTransactionDetails(Customer customerName);
+	
+}

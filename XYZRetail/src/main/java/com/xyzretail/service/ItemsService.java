8d4ae6ce239aacbee1c2ad616d421aacd53febe9
whoisvamshi <nagarapuvamshikrishna@gmail.com>
2022-07-29 12:13:59 +0530

    calculated tax

--- XYZRetail/src/main/java/com/xyzretail/service/ItemsService.java
+++ XYZRetail/src/main/java/com/xyzretail/service/ItemsService.java
@@ -3,12 +3,38 @@ package com.xyzretail.service;
 import java.util.List;
 
 import com.xyzretail.bean.Item;
+import com.xyzretail.client.Customer;
 
 public interface ItemsService {
-	double getBooksCost(int requiredBooks);
-	double getCdCost(int requiredCds);
-	double getCosmeticsCost(int requiredCosmetics);
-	double generateBill();
+//	double getBooksCost(int requiredBooks);
+//	double getCdCost(int requiredCds);
+//	double getCosmeticsCost(int requiredCosmetics);
+//	double generateBill();
 	List<Item> getAllItems();
-	Item searchItemsById(String id,int reqQuantity);
+   Item searchItemsById(String id,int reqQuantity);
+
+	boolean addItem(Item item);
+
+	boolean addCustomer(Customer customer);
+
+	List<Item> getItemDetails();
+
+	boolean removeCustomer(Customer customer);
+
+	double generateBill();
+
+	double getTax(Item item);
+
+	boolean validateCustomer();
+
+	double setTax(Item item);
+
+	double calculateBooksCost(Item item);
+
+	double calculateCDCost(Item item);
+
+	double calculateCosmeticsCost(Item item);
+
+	double calculateTaxAmount(Item category);
+
 }
--- XYZRetail/src/main/java/com/xyzretail/service/ItemsServiceImpl.java
+++ XYZRetail/src/main/java/com/xyzretail/service/ItemsServiceImpl.java
@@ -3,31 +3,101 @@ package com.xyzretail.service;
 import java.util.List;
 
 import com.xyzretail.bean.Item;
+import com.xyzretail.client.Customer;
 
 public class ItemsServiceImpl implements ItemsService {
 
 	@Override
-	public double getBooksCost(int requiredBooks) {
-		return 0;
+	public boolean addItem(Item item) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	@Override
-	public double getCdCost(int requiredCds) {
-		return 0;
+	public boolean addCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	@Override
-	public double getCosmeticsCost(int requiredCosmetics) {
-		return 0;
+	public List<Item> getItemDetails() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean removeCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	@Override
 	public double generateBill() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getTax(Item item) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean validateCustomer() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double setTax(Item item) {
+		double tax=0;
+		if(item.getItemName()=="books")
+		{
+	 tax=	item.getItemPrice()*0.10;
+		}
+		else if(item.getItemName()=="cosmetics")
+		{
+			 tax=item.getItemPrice()*0.30;
+		}
+		else if(item.getItemName()=="CD")
+		{
+			tax=item.getItemPrice()*0.20;
+		}
+		return tax;
+	}
+
+	@Override
+	public double calculateBooksCost(Item item) {
+
+		
+		return 0;
+	}
+
+	@Override
+	public double calculateCDCost(Item item) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double calculateCosmeticsCost(Item item) {
+
+		
+		
+		
+		return 0;
+	}
+
+	@Override
+	public double calculateTaxAmount(Item category) {
+		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public List<Item> getAllItems() {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -38,3 +108,5 @@ public class ItemsServiceImpl implements ItemsService {
 	}
 
 }
+
+

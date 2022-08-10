2ecf44d01dbb22638c803d4c13ca508a320caa8d
Rishabh Dev <rishabhdev444@gmail.com>
2022-08-10 09:02:16 +0530

    updated

--- XYZRetail/src/main/java/com/xyzretail/persistence/ItemsCartDaoImpl.java
+++ XYZRetail/src/main/java/com/xyzretail/persistence/ItemsCartDaoImpl.java
@@ -1,18 +1,9 @@
 package com.xyzretail.persistence;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import com.xyzretail.bean.*;

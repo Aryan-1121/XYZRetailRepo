cd5f089f13d724e70fb97e7646266fa6c84edeb9
Aryan <106900422+Aryan-1121@users.noreply.github.com>
2022-08-13 20:47:01 +0530

    Everything with jdbcTemplate except one

---
+++ XYZRetail7/src/main/java/com/xyzretail/service/CustomerServiceImpl.java
@@ -0,0 +1,52 @@
+package com.xyzretail.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.xyzretail.bean.Customer;
+import com.xyzretail.persistence.CustomerDao;
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+	private CustomerDao customerDao;
+	
+	@Autowired
+	public void setCustomerDao(CustomerDao customerDao) {
+		this.customerDao = customerDao;
+	}
+
+	@Override
+	public boolean addCustomer(Customer customer) {
+
+		return customerDao.addCustomer(customer);
+	}
+	@Override
+	public boolean validateCustomer(Customer customer) {
+//	return customerDao.validateCustomer(customer);
+	
+	Customer cus= customerDao.validateCustomer(customer);
+	if(cus.getUserName().equals(null))	 {
+		System.out.println("incorrect username");
+		return false;
+	}
+	
+	if(cus.getUserName().equals(customer.getUserName())) {
+		
+		if(cus.getUserPassword().equals(customer.getUserPassword())) 
+			return true;
+		
+		else
+			System.out.println("Check your Password and Try again :) ");
+		}
+	
+	else {
+			
+		System.out.println("Check your User Name and Try again :) ");
+		return false;
+	}
+	return false ;
+	}
+
+	
+
+}

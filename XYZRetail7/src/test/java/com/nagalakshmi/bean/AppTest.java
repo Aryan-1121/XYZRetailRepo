682279b050d02692dbff403b110969af3d506e20
Nagalakshmi S <nagal@LAPTOP-NEFDGL5>
2022-08-15 23:06:03 +0530

    complete chnages and pull fresh copy now

--- XYZRetail7/src/test/java/com/nagalakshmi/bean/AppTest.java
+++ XYZRetail7/src/test/java/com/nagalakshmi/bean/AppTest.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.nagalakshmi.bean;
 
 import static org.junit.Assert.assertTrue;
@@ -19,25 +19,3 @@ public class AppTest
         assertTrue( true );
     }
 }
-=======
-package com.nagalakshmi.bean;
-
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
-}
->>>>>>> branch 'master' of https://github.com/Nagalakshmi-S/XYZRetailRepo.git

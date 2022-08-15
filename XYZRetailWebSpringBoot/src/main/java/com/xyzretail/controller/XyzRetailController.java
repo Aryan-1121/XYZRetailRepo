a493031d6af97265ed0081b2d612628acd94799f
Aryan <aryanmaurya879@gmail.com>
2022-08-15 22:26:58 +0530

    added empty class

--- XYZRetailWebSpringBoot/src/main/java/com/xyzretail/controller/XyzRetailController.java
+++ XYZRetailWebSpringBoot/src/main/java/com/xyzretail/controller/XyzRetailController.java
@@ -1,9 +1,17 @@
 package com.xyzretail.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class XyzRetailController {
-
+//
+//	@RequestMapping("/showAllItems")
+//	public ModelAndView showAllItemsController() {
+//		ModelAndView modelAndView = new ModelAndView();
+//		
+//		
+//	}
 	
 }

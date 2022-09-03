7170d51f7527d76ebc355d65210b50caec3aebe3
Nagalakshmi S <nagalakshmi6689@gmail.com>
2022-09-03 21:13:25 +0530

    pushing cart

--- MicroService App/XYZRetail-cart-producer/src/main/java/com/xyzretail/bean/ItemsCart.java
+++ MicroService App/XYZRetail-cart-producer/src/main/java/com/xyzretail/bean/ItemsCart.java
@@ -1,14 +1,9 @@
 package com.xyzretail.bean;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,9 +17,10 @@ public class ItemsCart {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int cartid;
-	@OneToMany(mappedBy="item_Id")
-	private List<ItemDetail> itemDetail =new ArrayList<ItemDetail>();
-	private String user_Name;
+	private String itemId;
+	private String itemName;
+	private String unitPrice;
+	private String userName;
 	private int requiredQuantity;
 	private double salesTax;
 	private double totalCost;

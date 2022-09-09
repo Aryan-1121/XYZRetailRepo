a00e4811b6d191681282d5b1b442bf1307042f69
Nagalakshmi S <nagalakshmi6689@gmail.com>
2022-09-09 14:50:00 +0530

    order id added

--- MicroService App/OrderService/src/main/java/com/xyzretail/bean/Orders.java
+++ MicroService App/OrderService/src/main/java/com/xyzretail/bean/Orders.java
@@ -1,6 +1,8 @@
 package com.xyzretail.bean;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
@@ -14,6 +16,9 @@ import lombok.NoArgsConstructor;
 public class Orders {
 	
 	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+
+	private int OrderId;
 	private int transactionId;
 	private String itemId;
 	private int purchasedQuantity;

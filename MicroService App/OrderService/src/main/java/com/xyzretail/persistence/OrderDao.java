54e66d1eaed3cc789b5d0dfb18f0875cffc22781
Aryan <aryanmaurya879@gmail.com>
2022-09-11 05:21:21 +0530

    small things to do in orders (incomplete)

--- MicroService App/OrderService/src/main/java/com/xyzretail/persistence/OrderDao.java
+++ MicroService App/OrderService/src/main/java/com/xyzretail/persistence/OrderDao.java
@@ -1,6 +1,10 @@
 package com.xyzretail.persistence;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.xyzretail.bean.Orders;
@@ -8,4 +12,9 @@ import com.xyzretail.bean.Orders;
 @Repository
 public interface OrderDao extends JpaRepository<Orders, Integer> {
 
+	
+	@Modifying
+	@Transactional
+	@Query(value = "insert into orders (")
+	void saveOrder();
 }
